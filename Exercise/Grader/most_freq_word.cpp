45787cec77bea3b415be2d5baab1e83eac730b9c
Mysterioucz <chatrinyoonchalard@gmail.com>
2024-08-22 12:10:26 +0700

    updated: Grader

---
+++ Exercise/Grader/most_freq_word.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm> // std::max_element, std::min_element
+using namespace std;
+
+int main(){
+    int n;
+    string key;
+    unordered_map<string,int> m;
+    cin >> n;
+    for (int _ = 0; _ < n; _++){
+        cin >> key;
+        m[key]++; // It create and set value to 0 then +1
+        // if(m.find(key) != m.end()){
+        //     m[key]++;
+        // }else{
+        //     m[key] = 1;
+        // }
+    }
+    auto max_val = max_element(m.begin(),m.end(),[](const pair<string,int> &p1,const pair<string,int> &p2) {
+        if(p1.second == p2.second){
+            return (p1.first < p2.first);
+        }
+        return (p1.second < p2.second);
+    });// using Lambda Function [capture clause](parameter){return something (in this case return boolean)}
+    cout << (*max_val).first << " " << (*max_val).second << endl;
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -4,18 +4,8 @@ using namespace std;
 
 int main()
 {
-    array<int,5> a = {0};
-    int* arr = new int[5];
-    for(auto it = a.begin();it != a.end(); it++){
-        cout << *it;
-    }
-    cout << endl;
-    for (int i = 0; i < 5; i++) { 
-        cout << arr[i] << " "; 
-    } 
-    cout << arr.size() << endl;
-    // freeing-up memory space by deleting arr 
-    delete[] arr; 
+    bool res =  ("a"<"z");
+    cout << res << endl;
 
 
     return 0;
