cc041c78ca5469685b66c0cfda3bea10b2c9c9e7
Mysterioucz <chatrinyoonchalard@gmail.com>
2024-08-27 00:33:17 +0700

    updated: Grader problem

--- Exercise/Grader/member-multiply.cpp
+++ Exercise/Grader/member-multiply.cpp
@@ -5,7 +5,6 @@ using namespace std;
 void member_multiply(vector<int> &v,
                      vector<pair<vector<int>::iterator, int>> &multiply)
 {
-    // int count = 0;
     vector <int> res = v;
     sort(multiply.begin(),multiply.end(),[](const pair<vector<int>::iterator, int> &p1, const pair<vector<int>::iterator, int> &p2){
         return p1.first < p2.first;
@@ -17,24 +16,26 @@ void member_multiply(vector<int> &v,
     //     count += pair.second;
     // }
     //
-    // Sol2: The arithmatic of (res.begin() + (pair.first - v.begin()) + count) may wrong due to something?
-    // for (size_t i = 0; i < multiply.size(); ++i)
-    // {
-    //     pair<vector<int>::iterator, int> pair = multiply[i];
-    //     res.insert((res.begin() + (pair.first - v.begin()) + count), pair.second, *(pair.first));
-    //     count += pair.second;
-    // }
-    int m_index = 0, v_index = 0;
-    while( m_index < multiply.size() && v_index < v.size()){
-        if(m_index < multiply.size() && (multiply[m_index].first - v.begin() == v_index)){
-            res.insert(res.end(), multiply[m_index].second, v[v_index]);
-            m_index++;
-        }
-        if(v_index < v.size()){
-            res.push_back(v[v_index]);
-            v_index++;
-        }
+    // Sol2: The arithmatic of (res.begin() + (pair.first - v.begin()) + count) may wrong due to something? : Grader accept idk
+    int count = 0;
+    for (size_t i = 0; i < multiply.size(); ++i)
+    {
+        pair<vector<int>::iterator, int> pair = multiply[i];
+        res.insert((res.begin() + (pair.first - v.begin()) + count), pair.second, *(pair.first));
+        count += pair.second;
     }
+    // Sol3 : using index
+    // int m_index = 0, v_index = 0;
+    // while( m_index < multiply.size() && v_index < v.size()){
+    //     if(m_index < multiply.size() && (multiply[m_index].first - v.begin() == v_index)){
+    //         res.insert(res.end(), multiply[m_index].second, v[v_index]);
+    //         m_index++;
+    //     }
+    //     if(v_index < v.size()){
+    //         res.push_back(v[v_index]);
+    //         v_index++;
+    //     }
+    // }
     v = res;
 }
 int main()
