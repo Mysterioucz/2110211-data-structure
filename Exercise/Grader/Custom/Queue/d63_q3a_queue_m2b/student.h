ed38c88de19336e5aecc8969a385ad09573604d3
Mysterioucz <chatrinyoonchalard@gmail.com>
2024-10-05 09:01:10 +0700

    grader d63_q3a_queue_m2b

--- Exercise/Grader/Custom/Queue/d63_q3a_queue_m2b/student.h
+++ Exercise/Grader/Custom/Queue/d63_q3a_queue_m2b/student.h
@@ -8,6 +8,16 @@
 template <typename T>
 void CP::queue<T>::move_to_back(size_t pos) {
   //write your code here
+  T val = mData[(mFront + pos)%mCap];
+  for(int i = 0;i < mSize - pos;i++){
+    int p = (mFront + pos + i)%mCap;
+    if(p == mCap - 1){
+      mData[p] = mData[0];
+    }else{
+      mData[p] = mData[p + 1];
+    }
+  }
+  mData[(mFront + mSize - 1)%mCap] = val;
 }
 
 #endif
