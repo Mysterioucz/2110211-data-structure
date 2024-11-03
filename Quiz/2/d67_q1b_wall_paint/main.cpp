fabe5b3726e6ffc67c99fb0e8ba0b84d4cee17c1
Mysterioucz <chatrinyoonchalard@gmail.com>
2024-11-04 01:05:04 +0700

    Grader Wall Paint 65%

--- Quiz/2/d67_q1b_wall_paint/main.cpp
+++ Quiz/2/d67_q1b_wall_paint/main.cpp
@@ -1,10 +1,74 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <set>
 #include <map>
+#include <numeric>
 
 using namespace std;
 
 int main(){
     int  n,m,x,c,d;
+    cin >> n >> m;
+    m *= 2;
+    set<pair<int,int>> wall;
+    map<pair<int,int>,int> layer;
+    vector<pair<int,int>> paint(m/2 + 1);
+    while(m--){
+        cin >> x >> c >> d;
+        if(d){
+            paint[c].second = x;
+        }else{
+            paint[c].first = x;
+        }
+    }
+    int maxLayer = 0;
+    for(int i = 1;i < paint.size();i++){
+        int s = paint[i].first, e = paint[i].second;
+        // int validity = accumulate(wall.begin() + s,wall.begin() + e + 1,0);
+        // if(((wall[e+1] == wall [e])&&(wall[s-1] == wall[s])&&(validity == (wall[s])*(e - s + 1)) )|| validity == 0){
+        //     for(int j = s; j < e + 1;j++) wall[j]++;
+        //     maxLayer = maxLayer<wall[s] ? wall[s]:maxLayer;
+        // }
+        if(wall.empty()){
+            wall.insert({s,e});
+            layer[{s,e}] = 1;
+            maxLayer = 1;
+        }else{
+            auto it = wall.upper_bound({s,e});
+            if((it != wall.end())&&(e < it->first)){
+                if(it != wall.begin())it--;
+                else{
+                    wall.insert({s,e});
+                    layer[{s,e}] = 1;
+                    maxLayer = maxLayer < layer[{s,e}]  ? layer[{s,e}] :maxLayer;
+                    continue;
+                }
+                if((s > it->second)){ // between existing interval
+                    wall.insert({s,e});
+                    layer[{s,e}] = 1;
+                    maxLayer = maxLayer < layer[{s,e}]  ? layer[{s,e}] :maxLayer;
+                }else if((s > it->first)&&(e < it->second)){ // inside interval
+                    wall.insert({s,e});
+                    layer[{s,e}] = layer[*it] + 1;
+                    maxLayer = maxLayer < layer[{s,e}]  ? layer[{s,e}] :maxLayer;
+                }
+            }else{
+                it--;
+                if((s > it->first)&&(e < it->second)){
+                    wall.insert({s,e});
+                    layer[{s,e}] = layer[*it] + 1;
+                    maxLayer = maxLayer < layer[{s,e}]  ? layer[{s,e}] :maxLayer;
+                }else if(s > it->second ){
+                    wall.insert({s,e});
+                    layer[{s,e}] = 1;
+                    maxLayer = maxLayer < layer[{s,e}]  ? layer[{s,e}] :maxLayer;
+                }
+            }
+            
+        }
+    }
+    cout << maxLayer << endl;
+
     return 0;
 }
