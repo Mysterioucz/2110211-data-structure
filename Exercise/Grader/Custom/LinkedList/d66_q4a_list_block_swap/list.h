abf5665b7551cc9d1366ab85e72ad787d39b9bf8
Mysterioucz <chatrinyoonchalard@gmail.com>
2024-11-07 15:26:25 +0700

    Grader Done: Linked List Block Swap

--- Exercise/Grader/Custom/LinkedList/d66_q4a_list_block_swap/list.h
+++ Exercise/Grader/Custom/LinkedList/d66_q4a_list_block_swap/list.h
@@ -5,9 +5,6 @@
 #include <iostream>
 //#pragma once
 
-bool ableToCreateNode_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m = false;
-int numberOfListCreated_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m = 0;
-
 namespace CP {
 
 template <typename T>
@@ -22,18 +19,10 @@ class list
         node *next;
 
         node() :
-          data( T() ), prev( this ), next( this ) {
-			  if (numberOfListCreated_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m != 0 && ableToCreateNode_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m == false) {
-				throw std::out_of_range("can't create node");
-			  }
-		  }
+          data( T() ), prev( this ), next( this ) { }
 
         node(const T& data,node* prev, node* next) :
-          data ( T(data) ), prev( prev ), next( next ) {
-			  if (numberOfListCreated_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m != 0 && ableToCreateNode_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m == false) {
-				throw std::out_of_range("can't create node");
-			  }
-		  }
+          data ( T(data) ), prev( prev ), next( next ) { }
     };
 
     class list_iterator {
@@ -91,14 +80,11 @@ class list
       for (auto &x : *this) {
         push_back(x);
       }
-	  numberOfListCreated_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m++;
     }
 
     // default constructor
     list() :
-      mHeader( new node() ), mSize( 0 ) {
-		numberOfListCreated_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m++;
-	  }
+      mHeader( new node() ), mSize( 0 ) { }
 
     // copy assignment operator using copy-and-swap idiom
     list<T>& operator=(list<T> other) {
@@ -156,9 +142,6 @@ class list
     }
 
     iterator insert(iterator it,const T& element) {
-	  if (ableToCreateNode_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m == false) {
-		throw std::out_of_range("can't create node");
-	  }
       node *n = new node(element,it.ptr->prev, it.ptr);
       it.ptr->prev->next = n;
       it.ptr->prev = n;
@@ -180,27 +163,13 @@ class list
     }
 
     void print() {
-      for (iterator it = begin();it!=end(); it++) {
+      int i = 0;
+      iterator before;
+      for (iterator it = begin();it!=end();before = it, it++,i++) {
         std::cout << it.ptr->data << " ";
       }
-
-	  std::cout << "\n";
-
-      iterator it = end();
-	  for (it--;it!=end(); it--) {
-		  std::cout << it.ptr->data << " ";
-	  }
-
 	  std::cout << "\n";
     }
-
-	//-------------- For grader --------------
-	void setAbleToCreateNode(bool value, std::string key) {
-		if(key == "gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m")
-			ableToCreateNode_gEliyXy1WsQBUpm8EO2nK2HHUJt7Hu0m = value;
-		else
-			throw std::out_of_range("wrong key");
-	}
 
 	//-------------- For student.h --------------
 	void block_swap(iterator a1, iterator a2, iterator b1, iterator b2);
--- Exercise/Grader/Custom/LinkedList/d66_q4a_list_block_swap/student.h
+++ Exercise/Grader/Custom/LinkedList/d66_q4a_list_block_swap/student.h
@@ -8,52 +8,32 @@ void CP::list<T>::block_swap(iterator a1, iterator a2, iterator b1, iterator b2)
 	// write your code here
 	// TODO FIX THIS
 	if(a1==a2 && b1==b2)return;
-	else if(a1==a2 && b1!=a2){
-		auto tmp1 = b1.ptr, tmp2 = b2.ptr->prev;
-		b2.ptr->prev = b1.ptr->prev;
-		b1.ptr->prev->next = b2.ptr;
+	if(b1 == a2){
+		std::swap(a1,b1);
+		std::swap(a2,b2); // if a1==b2 this will broke
+	}
+	if(a1 == b2){
+		auto a_prev = a1.ptr->prev;
+    	auto b_prev = b1.ptr->prev;
+    	auto a_end = a2.ptr->prev;
+    	auto b_end = b2.ptr->prev;
 
-		a1.ptr->prev->next = tmp1;
-		tmp1->prev = a1.ptr->prev;
-		a1.ptr->prev = tmp2;
-		tmp2->next = a1.ptr;
-	}else if(b1==b2 && a1!=a2){
-		auto tmp1 = a1.ptr, tmp2 = a2.ptr->prev;
-		a2.ptr->prev = a1.ptr->prev;
-		a1.ptr->prev->next = a2.ptr;
+    	// Adjust pointers for swapping ranges `[a1, a2)` and `[b1, b2)`
+    	a_prev->next = a2.ptr; // fixed
+    	b_prev->next = a1.ptr;
+    	b_end->next = a2.ptr;
+    	a_end->next = b1.ptr;
 
-		b1.ptr->prev->next = tmp1;
-		tmp1->prev = b1.ptr->prev;
-		b1.ptr->prev = tmp2;
-		tmp2->next = b1.ptr;
-	}else if((a1 != a2)&&(b1 != b2)){
-		node* tmp1 = a2.ptr->prev; 
-		node* tmp2 = b2.ptr->prev;
-		a1.ptr->prev->next = b1.ptr;
-		b1.ptr->prev->next = a1.ptr;
-
-		b2.ptr->prev = (tmp1);
-		a2.ptr->prev = (tmp2);
-
-		if(a1 != b2){
-			tmp1->next = b2.ptr;
-			a1.ptr->prev = b1.ptr->prev;
-		}
-		else{
-			tmp1->next = b1.ptr;
-			a1.ptr->prev = tmp2;
-		}
-		if(a2 != b1){
-			tmp2->next = a2.ptr;
-			b1.ptr->prev = a2.ptr;
-		}
-		else{
-			tmp2->next = a1.ptr;
-			b1.ptr->prev = tmp1;
-		}
-		}
-	mHeader->next->prev = mHeader;
-	mHeader->prev->next = mHeader;
+    	// Adjust `prev` pointers
+    	a1.ptr->prev = b_prev;
+    	b1.ptr->prev = a_end;
+    	a2.ptr->prev = b_end;
+	}else{
+		std::swap(a1.ptr->prev->next,b1.ptr->prev->next);
+		std::swap(a1.ptr->prev,b1.ptr->prev); // if a1==b2 this will broke
+		std::swap(a2.ptr->prev->next,b2.ptr->prev->next);
+		std::swap(a2.ptr->prev,b2.ptr->prev);
+	}
+	
 }
-
 #endif
